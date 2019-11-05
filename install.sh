c84e856aee0c42a4fa1612b2975fac06f4a42f3d
lcontrerasv <leonardo.contreras@suez.com>
2019-11-05 17:15:29 -0300

    Avance en el instalador

--- install.sh
+++ install.sh
@@ -1,22 +1,45 @@
-#!/bin/sh
-
+#!/bin/bash
 # Script for install dotFiles
 
-deps=(
-  git
-  foobar
-)
+# common functions
+. ./common.lib
 
-echo "checking dependencies..."
+CHECK_DEPS=true
 
-for i in "${deps[@]}"
+while [ "$1" != "" ]; do
+  case $1 in
+    -nc | --no-check ) CHECK_DEPS=false
+                       ;;
+    -h | --help ) usage
+                  exit
+                  ;;
+  esac
+  shift
+done
+
+DEPS=(git curl yarn)
+
+if $CHECK_DEPS; then
+   check_deps
+fi
+# Install dotfiles
+info "Installing dotfiles"
+
+for dot in $(find -H . -maxdepth 1 -iname '.*' -not -path '*.git*' )
 do
-  echo -n "cheking $i..."
-  if command -v $i > /dev/null; then
-     echo "ok"
-  else
-     echo "not found, please install it"
+  if [ -f "${dot}" ]; then
+    info "Installing file $dot"
+    cp -f $dot ~/
+		if [ $? -eq 0 ]; then
+				success "file $dot installed"
+		else
+				fail "Error while instaling $dot"
+		fi 
+	elif [ -d "${dot}" ]; then
+		info "Installing dir $dot"
+		#cp -rf $dot ~/
+		info "Not yet implemented"
   fi
 done
 
-cp .zshrc ~
+success "Finished"
