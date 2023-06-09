fb472bf35c4807a5cef4cc9cf5a5d9cb74c923fa
lcontreras <leonardo.contrerasv@gmail.com>
2023-06-09 13:11:53 -0400

    FIX a script install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -13,66 +13,66 @@
 
 # 0.- Check ZSH
 if zsh --version > /dev/null; then
-  success("ZSH found! setting as default shell")
+  success "ZSH found! setting as default shell"
   chsh -s $(which zsh)
 else
-  fail("ZSH NOT found!")
+  fail "ZSH NOT found!"
 
 # 1.- Install Oh my ZSH
-info("Installing Oh my ZSH!...")
+info "Installing Oh my ZSH!..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-success("Oh my ZSH Installed")
+success "Oh my ZSH Installed"
 
 # 2.- kitty
-info("Installing Kitty...")
+info "Installing Kitty..."
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-success("Kitty Installed")
+success "Kitty Installed"
 
 # 3.- bat
-info("Installing BAT...")
+info "Installing BAT..."
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   sudo apt install bat
-  success("BAT Installed")
+  success "BAT Installed"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   brew install -q bat
-  success("BAT Installed")
+  success "BAT Installed"
 else
-  info("OS not detected! BAT is not installed!")
+  info "OS not detected! BAT is not installed!"
 fi
 
 # 4.- fzf
-info("Installing FZF...")
+info "Installing FZF..."
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
-success("FZF Installed")
+success "FZF Installed"
 
 # 5.- lsd
-info("Installing LSD...")
+info "Installing LSD..."
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   sudo apt install lsd
-  success("LSD Installed")
+  success "LSD Installed"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   brew install -q lsd
-  success("LSD Installed")
+  success "LSD Installed"
 else
-  info("OS not detected! LSD is not installed!")
+  info "OS not detected! LSD is not installed!"
 fi
 
 # 6.- neovim & nvchad
-info("Installing NeoVim...")
+info "Installing NeoVim..."
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   sudo apt install neovim
   git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
-  success("NeoVim Installed")
+  success "NeoVim Installed"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   brew install -q neovim
-  git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
-  success("NeoVim Installed")
+  git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 
+  success "NeoVim Installed"
 else
-  info("OS not detected! NeoVim is not installed!")
+  info "OS not detected! NeoVim is not installed!"
 fi
 
-success("Finish!")
+success "Finish!"
 
 msg() {
   printf "\r$1\n"
